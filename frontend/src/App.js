d41d49d89eae3509f5759d940b2f2884b19f77ba
MalcolmSmalls <smallsbeats@gmail.com>
2023-01-11 22:53:22 -0800

    Add new screens, fix navBar and begin adding homescreen based on data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,23 @@
 import React from "react"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+import { BrowserRouter, Routes, Route} from "react-router-dom"
+import HomeScreen from "./screens/HomeScreen"
+import BeatScreen from "./screens/BeatScreen"
 
 function App() {
   return (
     <>
-      <Header />
-      <main>
-        
-      </main>
-      <Footer />
+      <BrowserRouter>
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<HomeScreen />}/>
+            <Route path="/product/:id" element={<BeatScreen />}/>
+          </Routes>
+        </main>
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
---
+++ frontend/src/screens/BeatScreen.js
@@ -0,0 +1,6 @@
+import React from "react"
+import {Row, Col} from "react-bootstrap"
+
+export default function BeatScreen(){
+    
+}
